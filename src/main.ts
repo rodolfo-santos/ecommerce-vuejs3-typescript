10d2f2ff2341d3ed4011d44f7b600cfa7f5f7ff9
Rodolfo Santos <rodolfodossantos29@gmail.com>
2022-04-23 13:25:32 -0300

    🍍fix pinia example

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
 import { loadFonts } from './plugins/webfontloader';
+import { createPinia } from 'pinia';
+import './registerServiceWorker';
 
 loadFonts();
 
 const app = createApp(App);
 app.use(router);
 app.use(vuetify);
+app.use(createPinia());
 app.mount('#app');
