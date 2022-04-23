f98995b4321b878a207dd1277b3b3b3037bdd8c1
Rodolfo Santos <rodolfodossantos29@gmail.com>
2022-04-23 13:07:59 -0300

    🍍add pinia/store on project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(createPinia()).mount('#app');
